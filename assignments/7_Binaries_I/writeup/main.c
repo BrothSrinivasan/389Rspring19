527a60fa92e31a1d4aac31eca8f3cd07c1431bfb
Barath Srinivasan <barath5999@gmail.com>
2019-04-13 03:40:27 -0400

    hw 7 finished

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,31 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Barath Srinivasan
+ * Section: 0202
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Barath Srinivasan
  */
 
-/* your code goes here */
+#include <stdio.h>
+
+int main()
+{
+	int v1 = 0xfeedface;
+	int v2 = 0x1ceb00da;
+	
+	printf("%d\n", v1);
+	printf("%d\n", v2);
+	
+	v1 ^= v2;
+	v2 ^= v1;
+	v1 ^= v2;  
+	
+	printf("%d\n", v1);
+	printf("%d\n", v2);
+	
+	return 0;
+}
+
+// The code is XOR swap meant to switch the values of variable v1 and v2
